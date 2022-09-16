85331f362d4a76a7bd34b8de35013fdf3201ee57
developersung13 <developersung13@gmail.com>
2022-09-17 01:13:21 +0900

    응용컴퓨터프로그래밍 03주 차 튜터링 퀴즈 코드 추가

---
+++ application-computer-programming/week-03/callByReferenceExample.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+// Bubble sort
+void sorting(int* arr, int len) {
+    for (int k=0; k < len; k++)
+        for (int z=0; z < len-k-1; z++)
+            if (arr[z] > arr[z+1]) {
+                int temp = arr[z];
+                arr[z] = arr[z+1];
+                arr[z+1] = temp;
+            }
+}
+
+int main() {
+    int numArr[] = { 5, 4, 3, 2, 1 };
+    int arrSize = sizeof(numArr) / sizeof(numArr[0]);
+    
+    printf("Before:\t");
+    for (int k=0; k < arrSize; k++) printf("%d ", numArr[k]);
+    
+    sorting(numArr, arrSize); // call-by-reference
+    
+    printf("\nAfter:\t");
+    for (int k=0; k < arrSize; k++) printf("%d ", numArr[k]);
+    return 0;
+}
