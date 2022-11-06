2824385a662f68c990164511ceec8ea11d83c4c4
developersung13 <developersung13@gmail.com>
2022-11-06 14:52:43 +0900

    응용컴퓨터프로그래밍 11주 차 튜터링 예제 코드(3) 추가

---
+++ application-computer-programming/week-11/example-3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define DEBUG
+
+int main() {
+  int x = 5, y = 10;
+
+  printf("x = %d, y = %d\n", x, y);
+#ifdef DEBUG
+  x++;
+  y++;
+#endif
+  printf("x = %d, y = %d\n", x, y);
+  return 0;
+}
